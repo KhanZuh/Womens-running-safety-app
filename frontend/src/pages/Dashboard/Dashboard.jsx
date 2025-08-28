9adc59463ffc987b137cebcdd4bb313566813069
emma <e.ferrari@live.co.uk>
2025-08-28 12:48:00 +0100

    bug fix - import error

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { createSafetySession } from "../services/safetySessionService";
+import { createSafetySession } from "../../services/safetySession";
 
 
 export function Dashboard() {
