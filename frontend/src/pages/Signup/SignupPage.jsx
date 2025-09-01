18fa12e132895d3c53b952e3084aeaf9ca9cb0f0
emma <e.ferrari@live.co.uk>
2025-09-01 13:02:39 +0100

    Add extra form fields to include user preferences

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -6,6 +6,10 @@ import { signup } from "../../services/authentication";
 export function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [preferredTimeOfDay, setPreferredTimeOfDay] = useState("");
+  const [numberOfRunsPerWeek, setNumberOfRunsPerWeek] = useState(0);
+  const [preferredTerrainTypes, setPreferredTerrainTypes] = useState("");
+
   const navigate = useNavigate();
 
   async function handleSubmit(event) {
@@ -27,6 +31,18 @@ export function SignupPage() {
     setPassword(event.target.value);
   }
 
+  function handlePreferredTimeOfDayChange(event) {
+    setPreferredTimeOfDay(event.target.value);
+  }
+  
+  function handleNumberOfRunsPerWeekChange(event) {
+    setNumberOfRunsPerWeek(event.target.value);
+  }
+
+  function handlePreferredTerrainTypesChange(event) {
+    setPreferredTerrainTypes(event.target.value);
+  }
+
   return (
     <>
       <h2>Signup</h2>
@@ -46,6 +62,36 @@ export function SignupPage() {
           value={password}
           onChange={handlePasswordChange}
         />
+
+        <label htmlFor="preferredTimeOfDay">Preferred Time Of Day to Run:</label>
+        <input
+          id="preferredTimeOfDay"
+          type="text"
+          value={preferredTimeOfDay}
+          onChange={handlePreferredTimeOfDayChange}
+        />
+        <label htmlFor="numberOfRunsPerWeek">Number of Runs Per Week:</label>
+        <input
+          id="numberOfRunsPerWeek"
+          type="number"
+          value={numberOfRunsPerWeek}
+          onChange={handleNumberOfRunsPerWeekChange}
+        />
+        <label htmlFor="preferredTerrainTypes">Preferred Terrain Types:</label>
+        <select
+        id="preferredTerrainTypes"
+          type="text"
+          value={preferredTerrainTypes}
+          onChange={handlePreferredTerrainTypesChange}
+        >
+          <option value="Road">Road</option>
+          <option value="Trail">Trail</option>
+          <option value="Track">Track</option>
+        </select>
+          
+          
+        
+
         <input role="submit-button" id="submit" type="submit" value="Submit" />
       </form>
     </>
