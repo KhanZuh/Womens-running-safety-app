fb48efb36a6a3f49d2b7382d18ea3b43aa53cd46
elenamurgia <elenamurgia@yahoo.it>
2025-09-03 10:19:48 +0100

    Removed references to emergency contact

--- api/app.js
+++ api/app.js
@@ -5,7 +5,6 @@ const cron = require("node-cron");
 
 const usersRouter = require("./routes/users");
 const safetySessionRouter = require("./routes/safetySession");
-const emergencyContactsRouter = require("./routes/emergencyContacts");
 const quotesRouter = require("./routes/quotes");
 const authenticationRouter = require("./routes/authentication");
 const tokenChecker = require("./middleware/tokenChecker");
@@ -23,7 +22,6 @@ app.use(bodyParser.json());
 // API Routes
 app.use("/users", usersRouter);
 app.use("/safetySessions", safetySessionRouter);
-app.use("/emergencyContacts", emergencyContactsRouter);
 app.use("/quotes", quotesRouter);
 app.use("/tokens", authenticationRouter);
 
